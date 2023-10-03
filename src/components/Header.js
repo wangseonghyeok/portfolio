029c439cd987ab5032fd8f28e9e8351468800cbf
wang seong hyeok <wang95124770@gmail.com>
2023-10-03 22:24:22 +0900

    헤더 부분 진행

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,40 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function Header() {
-    // 모든 요소에 대해 작업하기 위해 선택자로 해당 클래스의 모든 요소를 가져옵니다.
-    const customSelects = document.querySelectorAll(".custom-sel");
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedValue, setSelectedValue] = useState(""); // 선택된 값을 관리
 
-    // 각 요소에 대한 루프를 실행합니다.
-    customSelects.forEach(function (selWrap) {
-        const selBtn = selWrap.querySelector("button");
-        const selLayer = selWrap.querySelector("ul");
-        const selResult = selWrap.querySelector("input");
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-        selBtn.addEventListener("click", function () {
-            if (selWrap.classList.contains("open")) {
-                selWrap.classList.toggle("open");
-                toggleSlide(selLayer);
-            } else {
-                // 모든 .custom-sel 요소의 'open' 클래스를 제거하고 그 하위 ul 요소를 닫습니다.
-                customSelects.forEach(function (item) {
-                    item.classList.remove("open");
-                    toggleSlide(item.querySelector("ul"));
-                });
+    const handleSelect = (value) => {
+        setSelectedValue(value);
+        setIsOpen(false);
+    };
 
-                selWrap.classList.add("open");
-                toggleSlide(selLayer);
-            }
-        });
-
-        // 각 li 요소에 대한 루프를 실행합니다.
-        selLayer.querySelectorAll("li").forEach(function (li) {
-            li.addEventListener("click", function () {
-                li.classList.add("selected");
-
-                // 선택된 항목을 제외한 다른 항목들의 'selected' 클래스를 제거합니다.
-                selLayer.querySelectorAll("li:not(.selected)").forEach(function (otherLi) {
-                    otherLi.classList.remove("selected");
-                });
-
-                selResult.value = li.getAttribute("data-value");
-                selBtn.textContent = li.textContent;
-                selWrap.classList.remove("open");
-                toggleSlide(selLayer);
-            });
-        });
-    });
-
-    // 슬라이드 업 또는 다운 함수를 정의합니다.
-    function toggleSlide(element) {
-        const computedStyle = getComputedStyle(element);
-
-        if (computedStyle.display === "none") {
-            element.style.display = "block";
-        } else {
-            element.style.display = "none";
-        }
-    }
     return (
-        <>
-            <header id="header" className="header">
-                <gnb className="gnb">
-                    <Link to="/project">Wang.</Link>
-                    <div className="row-sel">
-                        <div className="custom-sel">
-                            <input type="text"></input>
-                            <button type="button">project</button>
-                        </div>
+        <header id="header" className="header">
+            <div className="gnb">
+                <Link to="/project">Wang.</Link>
+                <div className="row-sel">
+                    <div className={`custom-sel ${isOpen ? "open" : ""}`}>
+                        <input type="text" className="blind" readOnly />
+                        <button type="button" onClick={toggleDropdown}>
+                            {selectedValue}
+                        </button>
+                        <ul>
+                            <li onClick={() => handleSelect("Project")}>
+                                <Link to="/project">Project</Link>
+                            </li>
+                            <li onClick={() => handleSelect("Profile")}>
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                        </ul>
                     </div>
-                </gnb>
-            </header>
-        </>
+                </div>
+            </div>
+        </header>
     );
 }
