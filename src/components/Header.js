ef25187dcf1b920b647340f2d1dbf7c5def80aea
DESKTOP-OCF9DDS\Wang <wang95124770@gmail.com>
2023-10-24 23:16:55 +0900

    헤더 애니메이션 완료 / 아이폰 위아래 스크롤 버그

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,285 +4,246 @@ import Wrapper from "../components/Wrapper";
 import { Link } from "react-router-dom";
 import Data from "../data.json";
 export default function Header({ filterItem, setItem, menuItems, itemCounts }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [kindOpen, setKindOpen] = useState(false);
-    const [selectedValue, setSelectedValue] = useState(getSelectedValueFromLocalStorage() || "Project");
-    const [catagoryValue, setCatagoryValue] = useState("All");
-    const [scroll, setScroll] = useState(false);
-    let prevScroll = 0;
-    let timer;
-    let getAllCount = Data.main.length;
+  const [isOpen, setIsOpen] = useState(false);
+  const [kindOpen, setKindOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(getSelectedValueFromLocalStorage() || "Project");
+  const [catagoryValue, setCatagoryValue] = useState("All");
+  const [scroll, setScroll] = useState(false);
+  let prevScroll = 0;
+  let timer;
+  let getAllCount = Data.main.length;
 
-    // const divs = gnb.querySelectorAll("div");
-    //새로고침 시 LocalStorage 데이터 유지
-    function saveSelectedValueToLocalStorage(value) {
-        localStorage.setItem("selectedValue", value);
-    }
-    function getSelectedValueFromLocalStorage() {
-        return localStorage.getItem("selectedValue");
-    }
+  //새로고침 시 LocalStorage 데이터 유지
+  function saveSelectedValueToLocalStorage(value) {
+    localStorage.setItem("selectedValue", value);
+  }
+  function getSelectedValueFromLocalStorage() {
+    return localStorage.getItem("selectedValue");
+  }
 
-    function handleScroll() {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        let mainScrollY = document.querySelector(".sub-section .list");
-        const currScroll = mainScrollY.scrollTop;
-        if (window.innerWidth <= 720) {
-            if (prevScroll > currScroll) {
-                timer = setTimeout(() => {
-                    setScroll(false);
-                }, 20);
-            } else {
-                timer = setTimeout(() => {
-                    setScroll(true);
-                }, 20);
-            }
-            prevScroll = currScroll;
-        }
+  function handleScroll() {
+    if (timer) {
+      clearTimeout(timer);
     }
-    function reRender() {
-        setTimeout(() => {
-            let currentScrollY = document.querySelector(".sub-section .list");
-            currentScrollY.addEventListener("scroll", function () {
-                if (timer) {
-                    clearTimeout(timer);
-                }
-                const currScroll = this.scrollTop;
-                if (window.innerWidth <= 720) {
-                    if (prevScroll > currScroll) {
-                        timer = setTimeout(() => {
-                            setScroll(false);
-                        }, 20);
-                    } else {
-                        timer = setTimeout(() => {
-                            setScroll(true);
-                        }, 20);
-                    }
-                    prevScroll = currScroll;
-                }
-            });
+    let mainScrollY = document.querySelector(".sub-section .list");
+    const currScroll = mainScrollY.scrollTop;
+    if (window.innerWidth <= 720) {
+      if (prevScroll > currScroll) {
+        timer = setTimeout(() => {
+          setScroll(false);
+          //   console.log("위로");
         }, 20);
+      } else {
+        timer = setTimeout(() => {
+          setScroll(true);
+          //   console.log("아래");
+        }, 20);
+      }
+      prevScroll = currScroll;
     }
-
-    function handleTop() {
+  }
+  function reRender() {
+    setTimeout(() => {
+      let currentScrollY = document.querySelector(".sub-section .list");
+      currentScrollY.addEventListener("scroll", function () {
         if (timer) {
-            clearTimeout(timer);
+          clearTimeout(timer);
         }
-        const catagorySelect = document.querySelectorAll(".select");
-        let mainTop = document.querySelector(".sub-section .list");
-        for (const catagorySelects of catagorySelect) {
-            catagorySelects.addEventListener("click", function () {
-                mainTop.scrollTo({ top: 0, behavior: "smooth" });
-                timer = setTimeout(() => {
-                    let gnb = document.querySelector(".gnb");
-                    gnb.classList.remove("minimized");
-                }, 1000);
-            });
+        const currScroll = this.scrollTop;
+        if (window.innerWidth <= 720) {
+          if (prevScroll > currScroll) {
+            timer = setTimeout(() => {
+              setScroll(false);
+              console.log("위로");
+            }, 20);
+          } else {
+            timer = setTimeout(() => {
+              setScroll(true);
+              console.log("아래");
+            }, 20);
+          }
+          prevScroll = currScroll;
         }
-    }
-
-    // function mobileCareerClickHandler() {
-    //     let gnb = document.querySelector(".gnb");
-    //     let divs = gnb.querySelectorAll("div");
-
-    //     gnb.style.width = window.innerWidth <= 720 && divs.length === 3 ? "198.94px" : "310px";
-    //     console.log("3개");
+      });
+    }, 25);
+    // if (timer) {
+    //   clearTimeout(timer);
     // }
-
-    function projectClickHandler() {
-        let gnb = document.querySelector(".gnb");
-        gnb.style.width = window.innerWidth >= 720 ? "481.16px" : "320px";
-    }
-
-    function careerClickHandler() {
-        let gnb = document.querySelector(".gnb");
-        gnb.style.width = window.innerWidth >= 720 ? "320px" : "198.94px";
-    }
-
-    function addEventListeners() {
-        let project = document.querySelector(".project");
-        let career = document.querySelector(".career");
-        let home = document.querySelector(".name");
-        project.addEventListener("click", projectClickHandler);
-        career.addEventListener("click", careerClickHandler);
-        // career.addEventListener("click", mobileCareerClickHandler);
-        home.addEventListener("click", projectClickHandler);
-    }
-    function removeEventListeners() {
-        let project = document.querySelector(".project");
-        let career = document.querySelector(".career");
-        let home = document.querySelector(".name");
-        project.removeEventListener("click", projectClickHandler);
-        career.removeEventListener("click", careerClickHandler);
-        home.removeEventListener("click", projectClickHandler);
-    }
-    function updateEventListeners() {
-        addEventListeners();
-        if (window.innerWidth >= 720) {
-        } else {
-            // removeEventListeners();
-        }
-    }
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-    const catagoryDropdown = () => {
-        setKindOpen(!kindOpen);
-    };
-    const handleSelect = (value) => {
-        setSelectedValue(value);
-        saveSelectedValueToLocalStorage(value);
-        if (isOpen) {
-            setIsOpen(!isOpen);
-        }
-    };
-    const handleCatagory = (value) => {
-        setCatagoryValue(value);
-        if (kindOpen) {
-            setKindOpen(!kindOpen);
-        }
-    };
-    const el = useRef();
-    const handleCloseSelect = (e) => {
-        if (isOpen && (!el.current || !el.current.contains(e.target))) setIsOpen(false);
-    };
-    const catagoryCloseSelect = (e) => {
-        if (kindOpen && (!el.current || !el.current.contains(e.target))) setKindOpen(false);
-    };
-
-    // const resizinHeader = () => {
-    //     /* gnb depth 클릭 시 "width" 값이 가변적으로 바뀔때 */
-
-    //     const gnb = document.querySelector(".gnb");
-    //     const project = document.querySelector(".project");
-    //     const career = document.querySelector(".career");
-    //     const name = document.querySelector(".name");
-    //     const divs = gnb.querySelectorAll("div");
-    //     if (window.innerWidth <= 720) {
-    //         project.addEventListener("click", () => {
-    //             gnb.style.width = "320px";
-    //         });
-    //         name.addEventListener("click", () => {
-    //             gnb.style.width = "320px";
-    //         });
-    //         career.addEventListener("click", () => {
-    //             gnb.style.width = "198.94px";
-    //         });
-    //         if (divs.length === 3) {
-    //             gnb.style.width = "198.94px";
-    //         }
+    // timer = setTimeout(() => {
+    //   let mainScrollY = document.querySelector(".sub-section .list");
+    //   const currScroll = mainScrollY.scrollTop;
+    //   if (window.innerWidth <= 720) {
+    //     if (prevScroll > currScroll) {
+    //       timer = setTimeout(() => {
+    //         setScroll(false);
+    //         console.log("위로");
+    //       }, 20);
+    //     } else {
+    //       timer = setTimeout(() => {
+    //         setScroll(true);
+    //         console.log("아래");
+    //       }, 20);
     //     }
-    //     if (window.innerWidth >= 720) {
-    //         project.addEventListener("click", () => {
-    //             gnb.style.width = "481.16px";
-    //             return;
-    //         });
-    //         career.addEventListener("click", () => {
-    //             gnb.style.width = "310px";
-    //         });
-    //     }
-    // };
-    useEffect(() => {
-        const gnb = document.querySelector(".gnb");
-        const divs = gnb.querySelectorAll("div");
-        console.log(divs);
-        window.addEventListener("click", handleCloseSelect);
-        window.addEventListener("click", catagoryCloseSelect);
-        // window.addEventListener("resize", resizinHeader);
-        // resizinHeader();
-        handleTop();
+    //     prevScroll = currScroll;
+    //   }
+    // }, 100);
+  }
 
-        updateEventListeners();
-        window.addEventListener("resize", updateEventListeners);
-    });
+  function handleTop() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const catagorySelect = document.querySelectorAll(".select");
+    let mainTop = document.querySelector(".sub-section .list");
+    for (const catagorySelects of catagorySelect) {
+      catagorySelects.addEventListener("click", function () {
+        mainTop.scrollTo({ top: 0, behavior: "smooth" });
+        timer = setTimeout(() => {
+          let gnb = document.querySelector(".gnb");
+          gnb.classList.remove("minimized");
+        }, 1000);
+      });
+    }
+  }
+  function projectClickHandler() {
+    let gnb = document.querySelector(".gnb");
+    let rowSel = document.querySelectorAll(".custom-sel");
+    let selCount = rowSel.length;
+    var windowWidth = window.innerWidth;
 
-    useEffect(() => {
-        window.onload = function () {
-            let currentScrollY = document.querySelector(".sub-section .list");
-            currentScrollY.addEventListener("scroll", handleScroll);
-            return () => {
-                window.removeEventListener("scroll", handleScroll);
-            };
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [setScroll]);
+    if (windowWidth >= 720 && selCount === 2) {
+      gnb.style.width = "480px";
+    } else if (windowWidth >= 720 && selCount === 1) {
+      gnb.style.width = "310px";
+    } else if (windowWidth <= 720 && selCount === 2) {
+      gnb.style.width = "330px";
+    } else if (windowWidth <= 720 && selCount === 1) {
+      gnb.style.width = "198px";
+    } else {
+      gnb.removeAttribute("style");
+    }
+  }
 
-    return (
-        <header id="header" className="header" ref={el}>
-            <div className={`gnb ${scroll ? "minimized" : ""}`}>
-                <div className="row-sel">
-                    <Link
-                        to="/project"
-                        className="name"
-                        onClick={() => {
-                            handleSelect("Project");
-                            reRender();
-                            handleTop();
-                        }}
-                    >
-                        Wang
-                    </Link>
-                    <Wrapper>
-                        <div className={`custom-sel ${isOpen ? "open" : ""}`}>
-                            <button type="button" onClick={toggleDropdown}>
-                                {selectedValue}
-                            </button>
-                            <ul className="list">
-                                <li
-                                    className="project"
-                                    onClick={() => {
-                                        handleSelect("Project");
-                                        reRender();
-                                        handleTop();
-                                    }}
-                                >
-                                    <Link to="/project">Project</Link>
-                                </li>
-                                <li className="career" onClick={() => handleSelect("Career")}>
-                                    <Link to="/career">Career</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </Wrapper>
-                    {selectedValue !== "Career" && (
-                        <Wrapper>
-                            <div className={`custom-sel ${kindOpen ? "open" : ""}`}>
-                                <button type="button" onClick={catagoryDropdown}>
-                                    {catagoryValue === "All" ? (
-                                        <span>
-                                            All<span className="count">{getAllCount}</span>
-                                        </span>
-                                    ) : (
-                                        <span>
-                                            {catagoryValue}
-                                            <span className="count">{itemCounts[catagoryValue]}</span>
-                                        </span>
-                                    )}{" "}
-                                </button>
-                                <ul className="list">
-                                    <li onClick={() => handleCatagory("All")} className="select">
-                                        <button type="button" onClick={() => setItem(Data.main)}>
-                                            <span>All</span>
-                                            <span className="count">{getAllCount}</span>
-                                        </button>
-                                    </li>
-                                    {menuItems.map((Val, id) => {
-                                        return (
-                                            <li onClick={() => handleCatagory(Val)} key={id} className="select">
-                                                <button type="button" onClick={() => filterItem(Val)}>
-                                                    <span>{Val}</span>
-                                                    <span className="count">{itemCounts[Val]}</span>
-                                                </button>
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </div>
-                        </Wrapper>
-                    )}
-                </div>
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  const catagoryDropdown = () => {
+    setKindOpen(!kindOpen);
+  };
+  const handleSelect = (value) => {
+    setSelectedValue(value);
+    saveSelectedValueToLocalStorage(value);
+    if (isOpen) {
+      setIsOpen(!isOpen);
+    }
+  };
+  const handleCatagory = (value) => {
+    setCatagoryValue(value);
+    if (kindOpen) {
+      setKindOpen(!kindOpen);
+    }
+  };
+  const el = useRef();
+  const handleCloseSelect = (e) => {
+    if (isOpen && (!el.current || !el.current.contains(e.target))) setIsOpen(false);
+  };
+  const catagoryCloseSelect = (e) => {
+    if (kindOpen && (!el.current || !el.current.contains(e.target))) setKindOpen(false);
+  };
+
+  useEffect(() => {
+    projectClickHandler();
+
+    window.addEventListener("click", handleCloseSelect);
+    window.addEventListener("click", catagoryCloseSelect);
+    window.addEventListener("resize", projectClickHandler);
+    handleTop();
+  });
+
+  useEffect(() => {
+    let currentScrollY = document.querySelector(".sub-section .list");
+    currentScrollY.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setScroll]);
+
+  return (
+    <header id="header" className="header" ref={el}>
+      <div className={`gnb ${scroll ? "minimized" : ""}`}>
+        <div className="row-sel">
+          <Link
+            to="/project"
+            className="name"
+            onClick={() => {
+              handleSelect("Project");
+              reRender();
+              handleTop();
+            }}
+          >
+            Wang
+          </Link>
+          <Wrapper>
+            <div className={`custom-sel ${isOpen ? "open" : ""}`}>
+              <button type="button" onClick={toggleDropdown}>
+                {selectedValue}
+              </button>
+              <ul className="list">
+                <li
+                  className="project"
+                  onClick={() => {
+                    handleSelect("Project");
+                    reRender();
+                    handleTop();
+                  }}
+                >
+                  <Link to="/project">Project</Link>
+                </li>
+                <li className="career" onClick={() => handleSelect("Career")}>
+                  <Link to="/career">Career</Link>
+                </li>
+              </ul>
             </div>
-        </header>
-    );
+          </Wrapper>
+          {selectedValue !== "Career" && (
+            <Wrapper>
+              <div className={`custom-sel ${kindOpen ? "open" : ""}`}>
+                <button type="button" onClick={catagoryDropdown}>
+                  {catagoryValue === "All" ? (
+                    <span>
+                      All<span className="count">{getAllCount}</span>
+                    </span>
+                  ) : (
+                    <span>
+                      {catagoryValue}
+                      <span className="count">{itemCounts[catagoryValue]}</span>
+                    </span>
+                  )}{" "}
+                </button>
+                <ul className="list">
+                  <li onClick={() => handleCatagory("All")} className="select">
+                    <button type="button" onClick={() => setItem(Data.main)}>
+                      <span>All</span>
+                      <span className="count">{getAllCount}</span>
+                    </button>
+                  </li>
+                  {menuItems.map((Val, id) => {
+                    return (
+                      <li onClick={() => handleCatagory(Val)} key={id} className="select">
+                        <button type="button" onClick={() => filterItem(Val)}>
+                          <span>{Val}</span>
+                          <span className="count">{itemCounts[Val]}</span>
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </Wrapper>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,76 +1,76 @@
 import React, { useEffect, useState } from "react";
 import Wrapper from "../components/Wrapper";
 export default function Main({ item }) {
-    const [isMobile, setisMobile] = useState(true);
-    const resizingHandler = () => {
-        if (window.innerWidth <= 720) {
-            setisMobile(false);
-        } else {
-            setisMobile(true);
+  const [isMobile, setisMobile] = useState(true);
+  const resizingHandler = () => {
+    if (window.innerWidth <= 720) {
+      setisMobile(false);
+    } else {
+      setisMobile(true);
 
-            let gnb = document.querySelector(".gnb");
-            gnb.classList.remove("minimized");
-        }
-    };
-    useEffect(() => {
-        function syncHeight() {
-            document.documentElement.style.setProperty("--window-inner-height", `${window.innerHeight}px`);
-        }
-        window.addEventListener("resize", resizingHandler);
-        window.addEventListener("resize", syncHeight);
-        const mediaQuery = window.matchMedia("screen and (max-width: 720px)");
-        let grid = document.querySelector(".list");
+      let gnb = document.querySelector(".gnb");
+      gnb.classList.remove("minimized");
+    }
+  };
+  useEffect(() => {
+    function syncHeight() {
+      document.documentElement.style.setProperty("--window-inner-height", `${window.innerHeight}px`);
+    }
+    window.addEventListener("resize", resizingHandler);
+    window.addEventListener("resize", syncHeight);
+    const mediaQuery = window.matchMedia("screen and (max-width: 720px)");
+    // let grid = document.querySelector(".list");
 
-        if (mediaQuery.matches) {
-            grid.classList.remove("grid");
-            setisMobile(false);
-            syncHeight();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isMobile]);
+    if (mediaQuery.matches) {
+      // grid.classList.remove("grid");
+      setisMobile(false);
+      syncHeight();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile]);
 
-    return (
-        <Wrapper>
-            <main>
-                <section className="main-section">
-                    <div className="sub-section">
-                        <div className={`list ${isMobile ? "grid" : ""}`}>
-                            <Wrapper>
-                                {[...item].reverse().map((Val) => {
-                                    return Val.isVisible ? (
-                                        <div className="pf-list-item" key={Val.id}>
-                                            <div className="pf-list-inner">
-                                                <a href={Val.link} target="_blank" className="pf-img" rel="noreferrer">
-                                                    <img src={Val.img} alt="img"></img>
-                                                </a>
-                                                <div className="pf-info">
-                                                    <div className="pf-info-bottom">
-                                                        <div className="project-skill">
-                                                            {Val.skile.map((name, index) => {
-                                                                return (
-                                                                    <span key={Val.id + index + 1}>
-                                                                        {index ? "" : ""} {name}
-                                                                    </span>
-                                                                );
-                                                            })}{" "}
-                                                        </div>
-                                                        <p className="project-name">{Val.title}</p>
-                                                        <p className="project-info">
-                                                            <span className="info-vlaue">{Val.brand} /</span>
-                                                            <span className="info-vlaue"> {Val.data} /</span>
-                                                            <span className="info-vlaue"> {Val.kind}</span>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ) : null;
-                                })}
-                            </Wrapper>
+  return (
+    <Wrapper>
+      <main>
+        <section className="main-section">
+          <div className="sub-section">
+            <div className={`list ${isMobile ? "grid" : ""}`}>
+              <Wrapper>
+                {[...item].reverse().map((Val) => {
+                  return Val.isVisible ? (
+                    <div className="pf-list-item" key={Val.id}>
+                      <div className="pf-list-inner">
+                        <a href={Val.link} target="_blank" className="pf-img" rel="noreferrer">
+                          <img src={Val.img} alt="img"></img>
+                        </a>
+                        <div className="pf-info">
+                          <div className="pf-info-bottom">
+                            <div className="project-skill">
+                              {Val.skile.map((name, index) => {
+                                return (
+                                  <span key={Val.id + index + 1}>
+                                    {index ? "" : ""} {name}
+                                  </span>
+                                );
+                              })}{" "}
+                            </div>
+                            <p className="project-name">{Val.title}</p>
+                            <p className="project-info">
+                              <span className="info-vlaue">{Val.brand} /</span>
+                              <span className="info-vlaue"> {Val.data} /</span>
+                              <span className="info-vlaue"> {Val.kind}</span>
+                            </p>
+                          </div>
                         </div>
+                      </div>
                     </div>
-                </section>
-            </main>
-        </Wrapper>
-    );
+                  ) : null;
+                })}
+              </Wrapper>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Wrapper>
+  );
 }
